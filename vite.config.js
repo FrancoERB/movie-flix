6f7020aafd3f84cea7d4ed9fd4b5232587403129
Franco Erben <francoerben@hotmail.com>
2024-02-17 12:50:50 -0300

    Hosting add

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://francoerb.github.io/movie-flix.github.io'
+  base:"https://francoerb.github.io/movie-flix"
 })
 
 
